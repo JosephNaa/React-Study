d3281a7f7b30effce0e9de8103c1504800b0d444
JosephNa <js.pekah@gmail.com>
2021-02-17 10:28:50 +0900

    apply immer

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -3,6 +3,7 @@ import CreateUser from './CreateUser';
 import UserList from './UserList';
 import Counter from './Counter'
 import useInputs from './useInputs'
+import produce from 'immer'
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는중...')
@@ -48,24 +49,35 @@ function reducer(state, action) {
     //     }
     //   }
     case 'CREATE_USER':
-      return {
-        inputs: initialState.inputs,
-        users: state.users.concat(action.user)
-      }
+      return produce(state, draft => {
+        draft.users.push(action.user)
+      })
+      // return {
+      //   // inputs: initialState.inputs,
+      //   users: state.users.concat(action.user)
+      // }
     case 'TOGGLE_USER':
-      return {
-        ...state,
-        users: state.users.map(user => 
-          user.id === action.id
-          ? { ...user, active: !user.active}
-          : user
-        )
-      }
+      return produce(state, draft => {
+        const user = draft.users.find(user => user.id === action.id)
+        user.active = !user.active
+      })
+      // return {
+      //   ...state,
+      //   users: state.users.map(user => 
+      //     user.id === action.id
+      //     ? { ...user, active: !user.active}
+      //     : user
+      //   )
+      // }
     case 'REMOVE_USER':
-      return {
-        ...state,
-        users: state.users.filter(user => user.id !== action.id)
-      }
+      return produce(state, draft => {
+        const index = draft.users.findIndex(user => user.id === action.id)
+        draft.users.splice(index, 1)
+      })
+      // return {
+      //   ...state,
+      //   users: state.users.filter(user => user.id !== action.id)
+      // }
     default:
       throw new Error('Unhandled action')
   }
@@ -75,13 +87,14 @@ export const UserDispatch = createContext(null)
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState)
-  const [form, onChange, reset] = useInputs({
-    username: '',
-    email: '',
-  })
-  const {username, email} = form
-  const nextId = useRef(4)
+  // const [form, onChange, reset] = useInputs({
+  //   username: '',
+  //   email: '',
+  // })
+  // const {username, email} = form
+  // const nextId = useRef(4)
   const {users} = state;
+
   // const {username, email} = state.inputs
 
   // const onChange = useCallback(e => {
@@ -93,18 +106,18 @@ function App() {
   //   })
   // }, [])
 
-  const onCreate = useCallback(() => {
-    dispatch({
-      type: 'CREATE_USER',
-      user: {
-        id: nextId.current,
-        username,
-        email,
-      }
-    })
-    nextId.current += 1
-    reset()
-  }, [username, email, reset])
+  // const onCreate = useCallback(() => {
+  //   dispatch({
+  //     type: 'CREATE_USER',
+  //     user: {
+  //       id: nextId.current,
+  //       username,
+  //       email,
+  //     }
+  //   })
+  //   nextId.current += 1
+  //   reset()
+  // }, [username, email, reset])
 
   // const onToggle = useCallback(id => {
   //   dispatch({
@@ -125,10 +138,10 @@ function App() {
   return (
     <UserDispatch.Provider value={dispatch}>
       <CreateUser
-        username={username}
-        email={email}
-        onChange={onChange}
-        onCreate={onCreate}
+        // username={username}
+        // email={email}
+        // onChange={onChange}
+        // onCreate={onCreate}
       />
       <UserList 
         users={users}
